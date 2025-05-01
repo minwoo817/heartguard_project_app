179f175b36d868c8731705d3463ccd235b3d1a1d
ithodol <ddmdjesz1@gmail.com>
2025-05-01 10:21:45 +0900

    05/01 feat 전은서 네이버 지도 셋팅값 수정 중

--- lib/HeartGuard/layout/mainapp.dart
+++ lib/HeartGuard/layout/mainapp.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:heartguard_project_app/HeartGuard/map/viewMap.dart';
 
 class MainApp extends StatefulWidget {
   @override
@@ -11,25 +10,6 @@ class MainApp extends StatefulWidget {
 class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:AppBar(
-
-      ),
-      body:Container(
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ViewMap()),
-                );
-              },
-              child: Text("지도"),
-            ),
-          ],
-        ),
-      ),
-    );
+    return Text("aa");
   }
 }
--- lib/HeartGuard/main.dart
+++ lib/HeartGuard/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:heartguard_project_app/HeartGuard/layout/myapp.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // 네이버 맵 초기화
-  await NaverMapSdk.instance.initialize(
-      clientId: 'fo1nlo8f10', // 클라이언트 ID
-      onAuthFailed: (error) {
-        print('네이버 지도 인증 실패: $error');
-      }
-  );
-
   runApp(MyApp());
 }
