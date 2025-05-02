1637481fc42f6174de86dbb33def1acf52bf55d0
ithodol <ddmdjesz1@gmail.com>
2025-05-02 16:01:37 +0900

    05/02 feat 전은서 현재 위치 gps 연결 완료

--- lib/HeartGuard/map/mapview.dart
+++ lib/HeartGuard/map/mapview.dart
@@ -1,20 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:heartguard_project_app/HeartGuard/layout/myappbar.dart';
 
-class MapView extends StatefulWidget{
+class MapView extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _MapViewState();
-  }
+  State<MapView> createState() => _MapViewState();
 }
 
-class _MapViewState extends State<MapView>{
+class _MapViewState extends State<MapView> {
+  late NaverMapController _mapController;
+  NLatLng? _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _requestLocationPermission();
+  }
+
+  Future<void> _requestLocationPermission() async {
+    final status = await Permission.location.request();
+    if (status.isGranted) {
+      _getCurrentLocation();
+    } else if (status.isDenied) {
+      print('위치 권한이 거부됨');
+    } else if (status.isPermanentlyDenied) {
+      openAppSettings(); // 설정 화면
+    }
+  }
+
+  Future<void> _getCurrentLocation() async {
+    Position pos = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    setState(() {
+      _currentPosition = NLatLng(pos.latitude, pos.longitude);
+    });
+
+    if (_mapController != null && _currentPosition != null) {
+      _mapController.updateCamera(
+        NCameraUpdate.scrollAndZoomTo(
+          target: _currentPosition!,
+          zoom: 15,
+        ),
+      );
+      // 마커
+      final marker = NMarker(
+        id: 'current_location_marker',
+        position: _currentPosition!,
+      );
+
+      _mapController.addOverlay(marker);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(),
-      body: NaverMap(), // NaverMap
+      body: NaverMap(
+        onMapReady: (controller) {
+          _mapController = controller;
+          if (_mapController != null && _currentPosition != null) {
+            _mapController.updateCamera(
+              NCameraUpdate.scrollAndZoomTo(
+                target: _currentPosition!,
+                zoom: 15,
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
