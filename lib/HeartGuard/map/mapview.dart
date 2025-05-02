f32c683c502614142cb8515f19f6b5f07a6af6ae
ithodol <ddmdjesz1@gmail.com>
2025-05-02 18:19:42 +0900

    05/02 feat 전은서 현재 위치 gps 수정 중

--- lib/HeartGuard/map/mapview.dart
+++ lib/HeartGuard/map/mapview.dart
@@ -12,6 +12,7 @@ class MapView extends StatefulWidget {
 class _MapViewState extends State<MapView> {
   late NaverMapController _mapController;
   NLatLng? _currentPosition;
+  NMarker? _currentMarker;
 
   @override
   void initState() {
@@ -26,33 +27,43 @@ class _MapViewState extends State<MapView> {
     } else if (status.isDenied) {
       print('위치 권한이 거부됨');
     } else if (status.isPermanentlyDenied) {
-      openAppSettings(); // 설정 화면
+      openAppSettings(); // 설정 화면으로 이동
     }
   }
 
   Future<void> _getCurrentLocation() async {
-    Position pos = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
-    );
-
-    setState(() {
-      _currentPosition = NLatLng(pos.latitude, pos.longitude);
-    });
-
-    if (_mapController != null && _currentPosition != null) {
-      _mapController.updateCamera(
-        NCameraUpdate.scrollAndZoomTo(
-          target: _currentPosition!,
-          zoom: 15,
-        ),
-      );
-      // 마커
-      final marker = NMarker(
-        id: 'current_location_marker',
-        position: _currentPosition!,
+    try {
+      Position pos = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high, // 좌표 가져오기
       );
 
-      _mapController.addOverlay(marker);
+      setState(() {
+        _currentPosition = NLatLng(pos.latitude, pos.longitude); // 위도 경도
+      });
+
+      // 비동기 필수
+
+
+
+      if (_mapController != null && _currentPosition != null) {
+        _mapController!.updateCamera(
+          NCameraUpdate.scrollAndZoomTo(
+            target: _currentPosition!,
+            zoom: 15,
+          ),
+        );
+        // 처음 한 번만 마커 생성
+        if (_currentMarker == null) {
+          final marker = NMarker(
+            id: 'current_location_marker',
+            position: _currentPosition!,
+          );
+          _currentMarker = marker;
+          _mapController.addOverlay(marker);
+        }
+      }
+    } catch (e) {
+      print('위치 가져오기 실패: $e');
     }
   }
 
@@ -60,18 +71,38 @@ class _MapViewState extends State<MapView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(),
-      body: NaverMap(
-        onMapReady: (controller) {
-          _mapController = controller;
-          if (_mapController != null && _currentPosition != null) {
-            _mapController.updateCamera(
-              NCameraUpdate.scrollAndZoomTo(
-                target: _currentPosition!,
-                zoom: 15,
+      body: Stack(
+        children: [
+          NaverMap(
+            onMapReady: (controller) {
+              _mapController = controller;
+              if (_currentPosition != null) {
+                _mapController!.updateCamera(
+                  NCameraUpdate.scrollAndZoomTo(
+                    target: _currentPosition!,
+                    zoom: 15,
+                  ),
+                );
+              }
+            },
+          ),
+          Positioned(
+            bottom: 30,
+            right: 30,
+            child: FloatingActionButton(
+              onPressed: _getCurrentLocation,
+              backgroundColor: Colors.white, // 배경색
+              foregroundColor: Color(0xFFfd4b85),  // 아이콘 색
+              elevation: 6,
+              mini: true,
+              child: Icon(
+                Icons.my_location,
+                size: 30,
               ),
-            );
-          }
-        },
+              tooltip: '현재 위치로 이동',
+            ),
+          )
+        ],
       ),
     );
   }
